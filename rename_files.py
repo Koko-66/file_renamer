0c6f3f3981ffa1475d16d8a662bcc27a96cb6084
Koko-666 <52763151+Koko-666@users.noreply.github.com>
2023-10-09 22:26:29 +0100

    Refactor to rename files in all subfolders.

--- rename_files.py
+++ rename_files.py
@@ -21,13 +21,13 @@ def extract_names_from_file(file_path):
     source_names = []
     target_names = []
     # loop over the rows and columns and crate two lsits
-    for i in range(2, m_row + 1):
+    for i in range(1, m_row + 1):
         # get cell object by row, column
         for j in range(1, m_col+1):
             cell_obj = sheet_obj.cell(row=i, column=j)
             if cell_obj.column == 1:
                 source_names.append(cell_obj.value)
-            else:
+            if cell_obj.column == 2 and cell_obj.value is not None:
                 target_names.append(cell_obj.value)
     names_dict = dict(zip(source_names, target_names))
     return names_dict
@@ -36,16 +36,22 @@ def extract_names_from_file(file_path):
 def rename_files(names_dict):
     """Rename files"""
     target_folder = input("Path to the files you want to rename: ")
+    # iterate through all subfolders in the directory
+    for root, dirs, files in os.walk(target_folder):
+        for file in files:
+            # get subfolder name
+            subfolder = os.path.join(root.split("\\")[-1])
+            # get subfolder/file name
+            folder_file_path = os.path.join(subfolder, file)
 
-    for file in os.listdir(target_folder):
-        # check if file in the dictionary
-        if file in names_dict.keys():
-            old_name = os.path.join(target_folder, file)
+            # check if subfolder/file in the dictionary keys (source)
+            if folder_file_path in names_dict.keys():
+                old_name = os.path.join(target_folder, folder_file_path)
+                new_name = os.path.join(target_folder, subfolder+"\\"+ names_dict[folder_file_path])
+                print(f"Old: {old_name} \n=>New: {new_name}")
 
-            new_name = os.path.join(target_folder, names_dict[file])
-
-            # rename the files
-            os.rename(old_name, new_name)
+                # rename the files
+                os.rename(old_name, new_name)
 
 
 def run():
